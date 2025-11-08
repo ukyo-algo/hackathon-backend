e2d47e69a1861447cba899ff3f8eb0075a63186b
ukyo-hayashi <ukyo.hayashi@gmail.com>
2025-11-09 00:30:17 +0900

    vercelとの接続コードを書いた

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from .database import get_db, engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Vercelとの接続
+origins = ["https://hackathon-frontend-theta.vercel.app"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/v1/ping")
@@ -18,3 +30,8 @@ def read_users(db: Session = Depends(get_db)):
     # users = db.query(models.User).all()
     # return users
     return {"message": "DB connection successful (setup example)"}
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World from FastAPI!"}
