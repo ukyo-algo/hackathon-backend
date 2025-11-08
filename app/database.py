e4d3b25a00fa8c6064d63d7c3720d93985b43ea7
ukyo-hayashi <ukyo.hayashi@gmail.com>
2025-11-09 00:02:28 +0900

    databaseに接続できるように変更(未テスト)

---
+++ app/database.py
@@ -0,0 +1,45 @@
+import os
+import sqlalchemy
+from google.cloud.sql.connector import Connector, IPTypes
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+connector = Connector()  # SQLとFastAPIをつなぐコネクタ
+
+DB_USER = os.environ["DB_USER"]
+DB_PASS = os.environ["DB_PASS"]
+DB_NAME = os.environ["DB_NAME"]
+INSTANCE_CONNECTION_NAME = os.environ["INSTANCE_CONNECTION_NAME"]
+
+
+def getconnection() -> sqlalchemy.engine.base.Connections:
+    """
+    Cloud SQL への接続を確立する関数.
+    """
+    conn = connector.connect(
+        INSTANCE_CONNECTION_NAME,
+        "pymysql",
+        user=DB_USER,
+        password=DB_PASS,
+        db=DB_NAME,
+    )
+    return conn
+
+
+engine = sqlalchemy.create_engine(
+    "mysql+pymysql://",
+    creator=getconnection,
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    """
+    DBセッションを取得するための依存関係.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from .database import get_db, engine, Base
 
 app = FastAPI()
 
@@ -9,3 +11,10 @@ def ping():
     疎通確認用のエンドポイント.
     """
     return {"status": "success"}
+
+
+@app.get("/users/")
+def read_users(db: Session = Depends(get_db)):
+    # users = db.query(models.User).all()
+    # return users
+    return {"message": "DB connection successful (setup example)"}
