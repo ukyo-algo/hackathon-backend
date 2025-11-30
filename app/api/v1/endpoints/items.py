bcbbedc768edd9f50ad5c7dcebb9598e64e2c1a1
ukyo-hayashi <ukyo.hayashi@gmail.com>
2025-11-30 22:57:11 +0900

    fix bug

--- app/api/v1/endpoints/items.py
+++ app/api/v1/endpoints/items.py
@@ -1,18 +1,15 @@
-from fastapi import (
-    APIRouter,
-    Depends,
-    HTTPException,
-    status,
-)  # ← HTTPExceptionとstatusを追加
+# hackathon-backend/app/api/v1/endpoints/items.py
+
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session, joinedload
 from typing import List
 
 from app.db.database import get_db
 from app.db import models
 from app.schemas import item as item_schema
-from app.api.v1.endpoints.users import get_current_user_dummy  # users.pyからインポート
 
-# このファイル用のAPIルーターを作成
+from .users import get_current_user_dummy
+
 router = APIRouter()
 
 
